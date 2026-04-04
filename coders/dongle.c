cf297ad11f8a73f2b4cebcf86f0b5e57edb57bb8
Nitwee <quentinrios1@gmail.com>
2026-04-04 18:09:10 +0200

    changed logic in dongle taking to fix bugs

--- coders/dongle.c
+++ coders/dongle.c
@@ -41,43 +41,6 @@ void	set_dongle_order(t_coder *coder, t_dongle **first, t_dongle **second)
 	}
 }
 
-int	try_get_dongle(t_coder *coder, t_dongle *dongle)
-{
-	int	added;
-
-	added = 0;
-	if (pthread_mutex_lock(&dongle->mutex) != 0)
-		return (0);
-	if (!coder_in_heap(&dongle->heap, coder))
-	{
-		if (!heap_add(&dongle->heap, coder))
-		{
-			pthread_mutex_unlock(&dongle->mutex);
-			return (0);
-		}
-		added = 1;
-	}
-	if (simulation_stopped(coder->data))
-	{
-		if (added)
-			remove_from_heap(&dongle->heap, coder);
-		pthread_mutex_unlock(&dongle->mutex);
-		return (0);
-	}
-	if (!can_take_dongle(dongle, coder))
-	{
-		if (added)
-			remove_from_heap(&dongle->heap, coder);
-		pthread_mutex_unlock(&dongle->mutex);
-		return (0);
-	}
-	heap_pop(&dongle->heap, coder->data->scheduler);
-	dongle->taken = 1;
-	pthread_mutex_unlock(&dongle->mutex);
-	log_action(coder, "has taken a dongle");
-	return (1);
-}
-
 int	get_dongle(t_coder *coder, t_dongle *dongle)
 {
 	struct	timespec ts;
@@ -124,11 +87,19 @@ int	take_dongles(t_coder *coder)
 	t_dongle	*first;
 	t_dongle	*second;
 
+	if (coder->data->number_of_coders == 1)
+	{
+		if (!get_dongle(coder, coder->left))
+			return (0);
+		while (!simulation_stopped(coder->data))
+			usleep(500);
+		release_dongle(coder->left, coder->data);
+		return (0);
+	}
 	set_dongle_order(coder, &first, &second);
-
 	if (!get_dongle(coder, first))
 		return (0);
-	if (!try_get_dongle(coder, second))
+	if (!get_dongle(coder, second))
 	{
 		release_dongle(first, coder->data);
 		return (0);
